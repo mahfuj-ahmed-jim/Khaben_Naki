810d4412db8548ae7cce4e69a7093b70bc0f9ea0
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2021-04-01 00:10:21 +0600

    Footer Created

---
+++ WebSite/footer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"></script>
+      
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+     
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+      
+    
+    <style>
+
+
+    </style>
+
+</head>
+<body>
+    
+
+    <footer>
+        <div style="background-color: #F15B5D;margin-top: 100px;">
+            <div class= "row pt-3  m-2 text-white">
+                <div class="col-lg-1 ">
+    
+                </div>
+            <div class="col-lg-5 pl-3 pr-2">
+                <div class="media p-2 ">
+                    <img src="img/khabennakilogo.png" alt="logo" style="width: 150px; height: 150px;">
+                </div>
+                
+                <h5 class="pl-3 font-weight bolder">Khaben Naki?</h5>
+                <h6>Bashundhara, Dhaka-1229, Bangladesh</h6>
+                <h6>Phone: +880191642x</h6>
+                <a href="https://www.google.com/maps/place/North+South+University/@23.81511,90.4255583,15z/data=!4m5!3m4!1s0x0:0xd660a4f50365294a!8m2!3d23.81511!4d90.4255583" target="_blank" class="btn btn-outline-light">Location</a><br><br>
+                
+    
+            </div>
+            <div class="col-lg-5 pt-4 ">
+                <h3 class="pt-5 font-weight bolder text-center">About Us</h3>
+                <p><h5 class="text-center">We are a small team of problem solvers, designers, thinkers and tinkers, working around the clock to make Khaben Naki?™ the most powerful online tool for Food Universe. We believe that world of food should be easy, fast and definitely fun! We wanted something simpler and broad, so we made it.</h5></p>  
+                <p class="pt-4 pl-5 text-center"> Copyright © Premier Noobs - 2021 All Rights Reserved.  </p>
+            </div>
+            <div class="col-lg-1">
+    
+                
+            </div>
+                </div>     
+        </div>
+    </footer>
+ 
+    
+    
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
