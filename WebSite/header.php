b982d133ab508ed7482603e20e5c60dc149cdd02
Rahemul Islam <rahemul.islam@northsouth.edu>
2021-04-05 23:52:55 +0600

    RestaurantsList.php formated & linked to the site

--- WebSite/header.php
+++ WebSite/header.php
@@ -87,7 +87,7 @@
                      <a class="nav-link" href="#">Hot Deals</a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="Restaurant.php">Restaurants</a>
+                     <a class="nav-link" href="RestaurantsList.php">Restaurants</a>
                   </li>
                   <li class="nav-item">
                      <a class="nav-link" href="#">Posts</a>
